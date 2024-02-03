1a83b68482e291e20d6cd7e3b091e16b7970dc96
JNCamil <juancamvh@hotmail.com>
2024-02-03 23:38:27 +0100

    review & another practice

--- SECCION1/Practica_seccion1.py
+++ SECCION1/Practica_seccion1.py
@@ -1,2 +1,4 @@
 print("El nombre de tu cerveza \nes \"" + input("¿Qué sabor tiene tu cerveza?") + " " +
       input("¿Qué identifica a tu cerveza?")+"\"")
+#Segundo ejemplo
+print("Tu edad es: " +input("¿Cuántos años tienes?")+" y mides: "  +input("¿Cuánto mides?"))
