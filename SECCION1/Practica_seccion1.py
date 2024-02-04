6ad53941936521a3cfbf8d7ca7f626442534a478
JNCamil <juancamvh@hotmail.com>
2024-02-04 20:42:39 +0100

    review pratice

--- SECCION1/Practica_seccion1.py
+++ SECCION1/Practica_seccion1.py
@@ -1,4 +1,7 @@
 print("El nombre de tu cerveza \nes \"" + input("¿Qué sabor tiene tu cerveza?") + " " +
       input("¿Qué identifica a tu cerveza?")+"\"")
-#Segundo ejemplo
+#Segunda práctica
 print("Tu edad es: " +input("¿Cuántos años tienes?")+" y mides: "  +input("¿Cuánto mides?"))
+#Tercera práctica
+print("Tú serás una combinación de:\n" + input("A qué se dedica tu amigo número 1:") + " y " + input ("A qué se dedica tu amigo número 2:"))
+
